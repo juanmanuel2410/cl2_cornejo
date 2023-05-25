6056fd6e852c5fb3b006e1c82515ef5c260777d4
JuanCornejo <i201811474@cibertec.edu.pe>
2023-05-24 21:20:00 -0500

    primer envio

---
+++ src/test/java/com/cornejo/cl2/Cl2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cornejo.cl2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
